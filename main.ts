dde65b5fd993a4c33989a39dd5d6bc507bf2b0cd
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-15 11:28:05 -0400

    neopixel code (NOT DONE)

--- main.ts
+++ main.ts
@@ -1,8 +1,60 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Peter Zerbinos
+ * Created on: Oct 2024
+ * This program will light up Neopixels depending on amount of light
 */
 
-basic.showString('Hello, World!')
+//if lightLevel <= 51
+
+input.onButtonPressed(Button.A, () => {
+    let lightLevel = input.lightLevel()
+    if (lightLevel <= 51) 
+    
+    {}
+})
+
+//if lightLevel > 52
+
+input.onButtonPressed(Button.A, () => {
+    let lightLevel = input.lightLevel()
+    if (lightLevel > 52)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    { }
+})
+
+//if lightLevel > 104
+
+input.onButtonPressed(Button.A, () => {
+    let lightLevel = input.lightLevel()
+    if (lightLevel > 104)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    { }
+})
+
+//if lightLevel > 156
+
+input.onButtonPressed(Button.A, () => {
+    let lightLevel = input.lightLevel()
+    if (lightLevel > 156)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    { }
+})
+
+//if lightLevel > 280
+
+input.onButtonPressed(Button.A, () => {
+    let lightLevel = input.lightLevel()
+    if (lightLevel > 280)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    { }
+})
